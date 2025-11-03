58b14aaf8dca03b6b7aecc65c3d2e1cf136d2b77
Joon <wjdwns011007@naver.com>
2025-11-03 17:55:33 +0900

    feat(lotto): Application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.domain.generator.LottoNumberGenerator;
+import lotto.domain.generator.RandomLottoGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoNumberGenerator generator = new RandomLottoGenerator();
+        LottoService service = new LottoService(generator);
+        LottoController controller = new LottoController(service);
+        controller.run();
     }
 }
+
